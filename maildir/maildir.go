2138c7cad7fdd56a2a06463a1d606c6f99adeaca
onozaty <onozaty@gmail.com>
2023-01-30 00:28:10 +0900

    メールフォルダの集計処理実装

--- maildir/maildir.go
+++ maildir/maildir.go
@@ -2,9 +2,66 @@ package maildir
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/emersion/go-imap/utf7"
 )
+
+type AggregateResult struct {
+	Name      string
+	Count     int32
+	TotalSize int64
+}
+
+func aggregateMailFolder(mailFolderPath string, mailFolderName string) (*AggregateResult, error) {
+
+	count := int32(0)
+	totalSize := int64(0)
+
+	// tmpにあるのは配送中のものなので対象から除いておく
+	for _, subName := range []string{"new", "cur"} {
+		subCount, subSize, err := aggregateMails(filepath.Join(mailFolderPath, subName))
+		if err != nil {
+			return nil, err
+		}
+		count += subCount
+		totalSize += subSize
+	}
+
+	return &AggregateResult{
+		Name:      mailFolderName,
+		Count:     count,
+		TotalSize: totalSize,
+	}, nil
+}
+
+func aggregateMails(dirPath string) (int32, int64, error) {
+
+	count := int32(0)
+	totalSize := int64(0)
+
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		info, err := entry.Info()
+		if err != nil {
+			return 0, 0, err
+		}
+
+		count++
+		totalSize += info.Size()
+	}
+
+	return count, totalSize, nil
+}
 
 func decodeFolderName(encodedName string) (string, error) {
 	decoder := utf7.Encoding.NewDecoder()
--- maildir/maildir_test.go
+++ maildir/maildir_test.go
@@ -1,10 +1,81 @@
 package maildir
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestAggregateMailFolder(t *testing.T) {
+
+	// ARRANGE
+	temp := t.TempDir()
+
+	{
+		new := createDir(t, temp, "new")
+		createFile(t, new, "1", "1")
+		createFile(t, new, "2", "1234567890")
+	}
+	{
+		cur := createDir(t, temp, "cur")
+		createFile(t, cur, "3", "123")
+		createFile(t, cur, "4", "1234")
+
+		// ディレクトリがあっても無視されること
+		curChild := createDir(t, cur, "child")
+		createFile(t, curChild, "a", "a")
+	}
+	{
+		// tmpは対象外
+		tmp := createDir(t, temp, "tmp")
+		createFile(t, tmp, "5", "1")
+	}
+
+	createFile(t, temp, "a", "a")
+	createFile(t, temp, "xxx", "xxx")
+
+	// ACT
+	result, err := aggregateMailFolder(temp, "INBOX")
+
+	// ASSERT
+	require.NoError(t, err)
+	assert.Equal(
+		t,
+		&AggregateResult{
+			Name:      "INBOX",
+			Count:     4,
+			TotalSize: 18,
+		},
+		result,
+	)
+}
+
+func TestAggregateMailFolder_SubFolderNotFound(t *testing.T) {
+
+	// ARRANGE
+	temp := t.TempDir()
+
+	{
+		// tmpは対象外
+		tmp := createDir(t, temp, "tmp")
+		createFile(t, tmp, "5", "1")
+	}
+
+	createFile(t, temp, "a", "a")
+	createFile(t, temp, "xxx", "xxx")
+
+	// ACT
+	_, err := aggregateMailFolder(temp, "INBOX")
+
+	// ASSERT
+	require.Error(t, err)
+	// OSによってエラーメッセージが異なるのでファイル名部分だけチェック
+	expect := "open " + filepath.Join(temp, "new")
+	assert.Contains(t, err.Error(), expect)
+}
 
 func TestDecodeFolderName(t *testing.T) {
 
@@ -29,4 +100,25 @@ func TestDecodeFolderName_DecodeError(t *testing.T) {
 
 	_, err := decodeFolderName("&AAA")
 	assert.EqualError(t, err, "&AAA is invalid folder name: utf7: invalid UTF-7")
+}
+
+func createDir(t *testing.T, parent string, name string) string {
+
+	dir := filepath.Join(parent, name)
+	err := os.Mkdir(dir, 0777)
+	require.NoError(t, err)
+
+	return dir
+}
+
+func createFile(t *testing.T, dir string, name string, content string) {
+
+	file, err := os.Create(filepath.Join(dir, name))
+	require.NoError(t, err)
+
+	_, err = file.Write([]byte(content))
+	require.NoError(t, err)
+
+	err = file.Close()
+	require.NoError(t, err)
 }
