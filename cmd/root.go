078ffc7e024b3287570e8960468145183e8fdf2f
onozaty <onozaty@gmail.com>
2023-01-29 10:06:39 +0900

    コマンドの雛形作成

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func newRootCmd() *cobra.Command {
+
+	rootCmd := &cobra.Command{
+		Use:   "maildir-stats",
+		Short: "report maildir statistics",
+		Run: func(cmd *cobra.Command, args []string) {
+			_ = cmd.Help()
+		},
+		SilenceErrors: true,
+		CompletionOptions: cobra.CompletionOptions{
+			HiddenDefaultCmd: true,
+		},
+	}
+
+	rootCmd.AddCommand(newVersionCmd())
+
+	for _, c := range rootCmd.Commands() {
+		// フラグ以外は受け付けないように
+		c.Args = func(cmd *cobra.Command, args []string) error {
+			if len(args) > 0 {
+				return fmt.Errorf("only flags can be specified")
+			}
+			return nil
+		}
+		c.Flags().SortFlags = false
+		c.InheritedFlags().SortFlags = false
+	}
+
+	return rootCmd
+}
+
+func Execute() {
+
+	rootCmd := newRootCmd()
+	cobra.CheckErr(rootCmd.Execute())
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/onozaty/maildir-stats/cmd"
+
+func main() {
+	cmd.Execute()
+}
